27ec9ae49492148da28744082fae6cffeac72a4d
Anshi Kandulna <24je0591@iitism.ac.in>
2024-12-01 00:28:24 +0530

    initial commit

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<"enter b: ";
+    cin>>b;
+    cout<<"addition = "<<a+b<<endl;
+    cout<<"subtraction = "<<a-b<<endl;
+    cout<<"multiplication = "<<a*b<<endl;
+    cout<<"division = "<<a/b<<endl;
+    cout<<"remainder = "<<a%b<<endl;
+}
